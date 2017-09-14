be22e88c5f1a0dac01c655219cc48e758669fea6
squallyaohao <31625585+squallyaohao@users.noreply.github.com>
2017-09-14 14:58:36 +0800

    department_manager wip3

--- department_manager.py
+++ department_manager.py
@@ -11,7 +11,7 @@ import department,member
 import pandas as pd
 import excelUtility
 
-
+overtimetablehead = [u'日期',u'姓名',u'加班项目',u'加班展项',u'加班时长',u'加班餐',u'加班描述']
 
 
 class DepartmentManager(Ui_MainWindow):
@@ -26,10 +26,15 @@ class DepartmentManager(Ui_MainWindow):
         self.query_fromdate.setDate(curDate)
         self.query_todate.setDate(curDate)
         self.projectDict = {}
+        self.allMembers = []
         self.getAllProject()
         self.getAllMembers()
+        #intialize all widgets
         self.drawList('project_list',self.projectDict)
         self.drawList('member_list',self.allMembers)
+        self.drawComboBox('query_member',self.allMembers)
+        self.drawComboBox('query_project',self.projectDict.keys())
+        self.query_overtime_table.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
         #set up connections
         self.setConnections()
         
@@ -44,6 +49,9 @@ class DepartmentManager(Ui_MainWindow):
         self.connect(self.delete_subproject_btn,QtCore.SIGNAL('clicked()'),self.deleteSubproject)
         self.connect(self.project_name,QtCore.SIGNAL('returnPressed()'),self.addProject)
         self.connect(self.subproject_name,QtCore.SIGNAL('returnPressed()'),self.addSubproject)
+        self.query_project.currentIndexChanged.connect(self.showSubprojectComobox)
+        self.connect(self.query,QtCore.SIGNAL('clicked()'),self.showQueryResult)
+        self.connect(self.save_excel,QtCore.SIGNAL('clicked()'),self.saveExcel)
         self.project_list.itemClicked.connect(self.showSubproject)
         #self.connect(self.project_list,QtCore.SIGNAL('doubleClicked()'),self.showSubproject)
  
@@ -135,13 +143,50 @@ class DepartmentManager(Ui_MainWindow):
         listWidget.clear()
         for temp in l:
             item = QtGui.QListWidgetItem(temp)
-            listWidget.addItem(item)            
+            listWidget.addItem(item)
+            
+            
+    def drawComboBox(self,widgetname='',l=[]):
+        comboBox = self.findChild(QtGui.QComboBox,widgetname)
+        comboBox.clear()
+        comboBox.addItem('*')
+        if len(l)>0:
+            for temp in l:
+                comboBox.addItem(temp)
+                
+                
+    def drawTable(self,tablename='',tableheader=[],tablelist=[]):
+        tableWidget = self.findChild(QtGui.QTableWidget,tablename)
+        numRows = len(tablelist)
+        numCols = len(tableheader)
+        print tablelist
+        tableWidget.setRowCount(numRows)
+        tableWidget.setColumnCount(numCols)        
+        tableWidget.setHorizontalHeaderLabels(tableheader)
+        textFont = QtGui.QFont('Hei',11)
+        for i,row in enumerate(tablelist):
+            for j,col in enumerate(row):
+                item = QtGui.QTableWidgetItem(unicode(col))
+                item.setTextAlignment(0x0004|0x0080)
+                item.setFont(textFont)
+                tableWidget.setItem(i,j,item)
+
 
             
     def showSubproject(self):
         currentProject = self.project_list.currentItem().text()
         subproject_list = self.projectDict[unicode(currentProject)]
         self.drawList('subproject_list',subproject_list)
+        
+    def showSubprojectComobox(self):
+        curProject = self.query_project.currentText()
+        if curProject != '*':
+            curSubproject = self.projectDict[unicode(curProject)]
+            self.drawComboBox('query_subproject',curSubproject)      
+        else:
+            self.drawComboBox('query_subproject',[])
+        
+        
     
     def getAllProject(self):
         self.projectDict = self.department.getProjectsFromServer('project')
@@ -151,10 +196,50 @@ class DepartmentManager(Ui_MainWindow):
         self.allMembers = self.department.getAllMembersFromServer('members')
         
         
+    def showQueryResult(self):
+        query_member = unicode(self.query_member.currentText())
+        query_fromdate = unicode(self.query_fromdate.text())
+        query_todate = unicode(self.query_todate.text())
+        query_project = unicode(self.query_project.currentText())
+        query_subproject = unicode(self.query_subproject.currentText())
+        result = self.department.queryOvertime(table='overtime', date=(query_fromdate,query_todate), member=query_member, 
+                                     project=query_project,subproject=query_subproject 
+                                     )
+        if result is not None:
+            self.drawTable(tablename='query_overtime_table',tableheader=overtimetablehead,tablelist=result)
         
         
     
-    
+    def saveExcel(self):
+        path = QtGui.QFileDialog.getSaveFileName(caption = 'Save Excel',filter="Excel File (*.xls *.xlsx)")
+        if path is not None:
+            curTable = []
+            curTable.append(overtimetablehead)
+            rows = self.query_overtime_table.rowCount()
+            cols = self.query_overtime_table.colorCount()
+            for i in range(0,rows):
+                temp = []
+                for j in range(0,cols):
+                    item = self.query_overtime_table.item(i,j)
+                    if j!=2:                 
+                        t = item.text()
+                        temp.append(unicode(t))
+                    else:
+                        comboItem = self.query_overtime_table.cellWidget(i,j)
+                        t = comboItem.currentText()
+                        temp.append(unicode(t))
+                curTable.append(temp)        
+            #catch error if IOError
+            error = excelUtility.exportToExcel(path,curTable)
+            if isinstance(error,IOError):
+                messagebox = QtGui.QMessageBox(2,QtCore.QString(u'错误'),QtCore.QString(u'保存文件失败，请检查文件名称或是否处于打开状态！'),QtGui.QMessageBox.Yes | QtGui.QMessageBox.Cancel)
+                messagebox.exec_()    
+            if error == 1:
+                messagebox = QtGui.QMessageBox(1,QtCore.QString(u''),QtCore.QString(u'文件保存成功！'),QtGui.QMessageBox.Yes)
+                messagebox.exec_() 
+        else:
+            messagebox = QtGui.QMessageBox(2,QtCore.QString(u'错误'),QtCore.QString(u'未指定文件保存路径！'),QtGui.QMessageBox.Yes | QtGui.QMessageBox.Cancel)
+            messagebox.exec_()   
         
         
         
